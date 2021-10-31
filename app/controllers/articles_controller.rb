7fda6a3de5de620e8e54c8ce0ef11d3f7fd96876
KseniiaMarkiv <ksenya.markiv@gmail.com>
2021-10-31 18:06:47 +0200

    Add auth methods to controller as helper methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,7 +17,7 @@ class ArticlesController < ApplicationController
    
   def create 
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = current_user
     # ---- see details in all_themes.docx 90-91 ----
     # render plain: @article # If we want look at Object 
     # render plain: @article.inspect # If we want look at details 
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   def home
     # render html: 'Hello World!'  - it's how you can check work of CTRL
+    redirect_to articles_path if logged_in?
   end  
   def about
   end
